b45718f786af94af57ec32aab8550149cfab091b
marumaro-git <sugawaramakotohiromu@sugawaramaMacBook-Pro.local>
2024-06-04 02:23:45 +0900

    sampleの緯度経度修正

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,6 +4,6 @@
 touch .env.local
 
 echo "api_key=\"\"" >> .env.local
-echo "lat=35.72967696209984" >> .env.local
-echo "lon=139.71084645217894" >> .env.local
+echo "lat=35.6811081" >> .env.local
+echo "lon=139.764516" >> .env.local
 echo "slack_web_hook_url=\"\"" >> .env.local
